8d9de3e48972d246ac24258ebc34cf457bb4d3bc
iirvine <emailrefugee@yahoo.com>
2012-02-06 14:54:22 -0700

    testing the sad path

---
+++ TDD/TDD.Tests/AveragingCalculator_Tests/SadPath/When_passing_null_measurements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD.Tests.AveragingCalculator_Tests.SadPath
+{
+	[TestClass]
+	public class When_passing_null_measurements
+	{
+		private AveragingCalculator averagingCalculator;
+
+		[TestInitialize]
+		public void SetUp()
+		{
+			averagingCalculator = new AveragingCalculator();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void When_measurements_are_null()
+		{
+			averagingCalculator.Aggregate(null);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void When_measurement_is_null()
+		{
+			var measurements = Mother.Get4Measurements();
+			measurements.Add(null);
+			averagingCalculator.Aggregate(measurements);
+		}
+	}
+}
