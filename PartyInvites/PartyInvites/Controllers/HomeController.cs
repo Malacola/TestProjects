cd05281f68aac3ba2badb1e7efc52719b8c092f4
iirvine <emailrefugee@yahoo.com>
2012-02-07 15:51:06 -0700

    PartyInvites

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
+
+    }
+}
