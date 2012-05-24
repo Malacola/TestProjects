a5e32139afd1dbf86cf368daabb5fa4f2f56fa67
iirvine <emailrefugee@yahoo.com>
2012-05-24 16:34:13 -0600

    messin with backbone

---
+++ LeafletFunTimes/scripts/EvLayer.js
@@ -0,0 +1,3 @@
+EvLayer = L.GeoJSON.Extend({
+	
+});
---
+++ LeafletFunTimes/scripts/MapProviders.Leaflet.js
@@ -0,0 +1,41 @@
+var App = App || {};
+App.MapProviders = App.MapProviders || {};
+
+App.MapProviders.Leaflet = function(){
+	
+	createMap = function(elementId){
+		this.map = new L.Map(elementId);
+		this.addBaseMap();
+		return map;
+	};
+
+	addBaseMap = function() {
+		var baseMap = new L.TileLayer("http://{s}.tiles.mapbox.com/v3/mapbox.mapbox-light/{z}/{x}/{y}.png", {
+            attribution: 'Map data by MapBox',
+            maxZoom: 18
+        });
+        this.addLayer(baseMap);
+	};
+
+	addLayer = function(layer){
+		this.map.addLayer(layer);
+
+	};
+
+	setView = function(options){
+		this.map.setView(new L.LatLng(options.lat, options.lon), options.zoom)
+
+	};
+
+	return {
+		createMap: createMap,
+		addBaseMap: addBaseMap,
+		addLayer: addLayer,
+		setView: setView
+	}
+};
+
+App.MapProviders.Leaflet.Layers = function(){
+
+};
+
